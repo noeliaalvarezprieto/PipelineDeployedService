c768052a29391d635981b54520aedddbaf72bbe9
noeliaalvarezprieto <noeliaalvarezprieto@gmail.com>
2026-03-26 15:00:23 +0100

    fix: remove unused response variable in indexController.test.js

--- src/__tests__/unit/indexController.test.js
+++ src/__tests__/unit/indexController.test.js
@@ -12,7 +12,6 @@ describe('Index Controller', () => {
     indexController.getIndex(req, res);
 
     expect(res.json).toHaveBeenCalled();
-    const response = res.json.mock.calls[0][0];
     expect(response).toHaveProperty('message');
     expect(response).toHaveProperty('environment');
     expect(response).toHaveProperty('timestamp');
